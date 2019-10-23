211a322ecee877ccd5e413f8c95f4583da1ed0bd
lyubavall <lapteva.l.l@yandex.ru>
2019-10-24 00:37:05 +0700

    Задача CSV, без поддержки доп. символов

---
+++ CSV/src/ru/academits/lapteva/csv/CSV.java
@@ -0,0 +1,92 @@
+package ru.academits.lapteva.csv;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class CSV {
+    public static void main(String[] args) {
+        String filePath = "CSV/src/ru/academits/lapteva/csv/file.csv";
+        String fileOutPath = "CSV/src/ru/academits/lapteva/csv/file.html";
+
+        getHTMLFromCSV(filePath, fileOutPath);
+    }
+
+    private static void getHTMLFromCSV(String filePath, String fileOutPath) {
+        try (Scanner scanner = new Scanner(new FileInputStream(filePath));
+             PrintWriter writer = new PrintWriter(fileOutPath)) {
+
+            writer.println("<table border=\"1\" cellpadding=\"5\">");
+
+            boolean isNewCell = true;
+            boolean isNewRow = true;
+            int DoubleQuotesCount = 0;
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+                if (isNewRow) {
+                    writer.println("<tr>");
+                }
+
+                for (int i = 0; i < line.length(); ++i) {
+                    char character = line.charAt(i);
+
+                    if (isNewCell) {
+                        writer.print("<td>");
+
+                        DoubleQuotesCount = 0;
+                    }
+
+                    if (character == ',') {
+                        if (i == line.length() - 1) {
+                            writer.print("<td>");
+
+                            isNewCell = true;
+                        } else if (DoubleQuotesCount % 2 != 0) {
+                            writer.print(',');
+                        } else {
+                            writer.print("</td>");
+
+                            isNewCell = true;
+                        }
+
+                    } else if (character == '"') {
+                        if (DoubleQuotesCount % 2 == 0 && DoubleQuotesCount != 0) {
+                            writer.print("&quot");
+                        }
+
+                        isNewCell = false;
+                        ++DoubleQuotesCount;
+
+                    } else {
+                        writer.print(character);
+
+                        isNewCell = false;
+                    }
+
+                    if (i == line.length() - 1) {
+                        if (DoubleQuotesCount % 2 != 0) {
+                            writer.println("<br>");
+
+                            isNewCell = false;
+                            isNewRow = false;
+
+                        } else {
+                            isNewCell = true;
+                            isNewRow = true;
+
+                            writer.print("</td>");
+                            writer.println("</tr>");
+                        }
+                    }
+                }
+            }
+
+            writer.println("</table>");
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+    }
+}
